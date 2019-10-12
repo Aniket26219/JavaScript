35a856b931fe7a6bc8a91929e2e3cd611e11bd33
ANIKET <aniket98shahane@gmail.com>
2019-10-12 16:43:17 +0530

    Arithematic operations completed

---
+++ Arithematics.js
@@ -0,0 +1,20 @@
+function add(num1,num2){
+    console.log(num1+num2);
+}
+
+function sub(num1,num2){
+    console.log(num1-num2);
+}
+
+function mul(num1,num2){
+    console.log(num1*num2);
+}
+
+function div(num1,num2){
+    console.log(num1/num2);
+}
+
+add(23,54)
+sub(43,12)
+mul(54,85)
+div(73,63)
