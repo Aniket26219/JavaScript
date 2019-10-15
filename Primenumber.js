e2c365df1b85c6226a749f9dcf1cd06b6586f022
ANIKET <aniket98shahane@gmail.com>
2019-10-15 14:58:42 +0530

    Prime number updated

--- Primenumber.js
+++ Primenumber.js
@@ -1,11 +1,11 @@
 function prime(num){
     if (num==1) {
-        console.log("it is not a prime number");
+        console.log("it is not a prime number");++
     } else if(num==2) {
         console.log("it is a prime number");
     }
     else{
-        for(var i=2;i<=num;i++){
+        for(var i=2;i<num;i++){
             if(i%num==0){
                 console.log("it is not a prime number");
             }
