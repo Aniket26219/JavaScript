c482d0220ef28a93e904884c5391eabf3b37200b
ANIKET <aniket98shahane@gmail.com>
2019-10-15 15:30:32 +0530

    Equality operator practice

---
+++ Practice/Practice2.js
@@ -0,0 +1,14 @@
+function normalequality(num){
+    if(num == '10'){
+        return true;
+    }
+    return false;
+}
+console.log(normalequality(10));
+function strictequality(numb){
+    if(numb==='10'){
+        return true;
+    }
+    return false;
+}
+console.log(strictequality(10));
