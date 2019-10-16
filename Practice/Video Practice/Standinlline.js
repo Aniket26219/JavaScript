e5a1f8f72b0151e45abf22778ddb8dc152cc1337
ANIKET <aniket98shahane@gmail.com>
2019-10-16 10:52:41 +0530

    advanced queue program

--- Practice/Video Practice/Standinlline.js
+++ Practice/Video Practice/Standinlline.js
@@ -1,8 +1,30 @@
-function queue(arr ,item){
-    arr.push(item);
-    return arr.shift();
+var array=[1,2,3,4,5,6,7,8,9,10];
+function advancedqueue(){
+    for(var i=10;i>=1;i--){
+        function queue(arr ,item){
+            arr.push(item);
+            return arr.shift();
+        }
+        console.log("Before : "+JSON.stringify(array));
+        console.log(queue(array, i));
+        console.log("After : "+JSON.stringify(array));    
+    }
 }
-var myArray=[1,2,3,4,5,6,7,8];
-console.log("Before : "+JSON.stringify(myArray));
-console.log(queue(myArray, 9));
-console.log("After : "+JSON.stringify(myArray));
+advancedqueue();
+
+
+
+
+
+
+
+
+
+// function queue(arr ,item){
+//     arr.push(item);
+//     return arr.shift();
+// }
+// var myArray=[1,2,3,4,5,6,7,8];
+// console.log("Before : "+JSON.stringify(myArray));
+// console.log(queue(myArray, 9));
+// console.log("After : "+JSON.stringify(myArray));
