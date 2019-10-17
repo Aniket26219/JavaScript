2e0033d6f9528eb953a4163982a95054b5eabcee
ANIKET <aniket98shahane@gmail.com>
2019-10-17 14:18:39 +0530

    video program of cards has been done

---
+++ Video Practice/Cards.js
@@ -0,0 +1,25 @@
+function cards(card){
+    var count=0;
+    switch(card){
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 10:
+        case 'A':
+        case 'K':
+        case 'Q':
+        case 'J':
+            count--;
+            break;
+    }
+    var holdbet = "hold";
+    if (count>0) {
+        holdbet="bet";
+    }
+    return count + " " + holdbet;
+}
+console.log(cards('A'));
